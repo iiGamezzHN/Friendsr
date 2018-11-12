ab81001ad24ac16696d58ccb1a03488174ac1845
[iiGamezzHN] <[davidarisz@gmail.com]>
2018-11-12 13:04:33 +0100

    Created FriendsAdapter.java

---
+++ app/src/main/java/com/example/cpuga/friendsr/FriendsAdapter.java
@@ -0,0 +1,26 @@
+package com.example.cpuga.friendsr;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendsAdapter extends ArrayAdapter<Friend> {
+
+    public FriendsAdapter(Context context, int resource, ArrayList<Friend> objects) {
+        super(context, resource, objects);
+    }
+
+    @androidx.annotation.NonNull
+    @Override
+    public View getView(int position, @androidx.annotation.Nullable View convertView, @androidx.annotation.NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item, parent, false);
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/cpuga/friendsr/MainActivity.java
+++ app/src/main/java/com/example/cpuga/friendsr/MainActivity.java
@@ -12,15 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Friend Aang = new Friend("Aang", "Airbender",getResources().getIdentifier("Aang","drawable","android:drawable"));
-        Friend Katara = new Friend("Katara", "Waterbender",getResources().getIdentifier("Katara","drawable","android:drawable"));
-        Friend Sokka = new Friend("Sokka", "Warrior",getResources().getIdentifier("Sokka","drawable","android:drawable"));
-        Friend Toph = new Friend("Toph", "Earthbender",getResources().getIdentifier("Toph","drawable","android:drawable"));
-        Friend Zuko = new Friend("Zuko", "Firebender",getResources().getIdentifier("Zuko","drawable","android:drawable"));
-        Friend Iroh = new Friend("Iroh", "Firebender",getResources().getIdentifier("Iroh","drawable","android:drawable"));
-        Friend Azula = new Friend("Azula", "Princess",getResources().getIdentifier("Azula","drawable","android:drawable"));
-        Friend Ozai = new Friend("Ozai", "Firelord",getResources().getIdentifier("Firelord","drawable","android:drawable"));
-        Friend Appa = new Friend("Appa", "Steed",getResources().getIdentifier("Appa","drawable","android:drawable"));
+        Friend Aang = new Friend("Aang", "Airbender",getResources().getIdentifier("aang","drawable","android:drawable"));
+        Friend Katara = new Friend("Katara", "Waterbender",getResources().getIdentifier("katara","drawable","android:drawable"));
+        Friend Sokka = new Friend("Sokka", "Warrior",getResources().getIdentifier("sokka","drawable","android:drawable"));
+        Friend Toph = new Friend("Toph", "Earthbender",getResources().getIdentifier("toph","drawable","android:drawable"));
+        Friend Zuko = new Friend("Zuko", "Firebender",getResources().getIdentifier("zuko","drawable","android:drawable"));
+        Friend Iroh = new Friend("Iroh", "Firebender",getResources().getIdentifier("iroh","drawable","android:drawable"));
+        Friend Azula = new Friend("Azula", "Princess",getResources().getIdentifier("azula","drawable","android:drawable"));
+        Friend Ozai = new Friend("Ozai", "Firelord",getResources().getIdentifier("ozai","drawable","android:drawable"));
+        Friend Appa = new Friend("Appa", "Steed",getResources().getIdentifier("appa","drawable","android:drawable"));
+
     }
 
     ArrayList<Friend> friends = new ArrayList<>();
